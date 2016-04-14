c106fa643178d4b2ac28b8f205b9e0c748d68903
Ben Goltz <ben.goltz.89@gmail.com>
2016-04-13 21:48:52 -0500

    migrated create-express-router.js to typescript

---
+++ lib/create-express-router.ts
@@ -0,0 +1,31 @@
+import * as express from "express"
+import { parseFile } from "./parse-file"
+
+export function createExpressRouter(path: string, cb: Function) {
+  var router = express.Router()
+  
+  parseFile(path).then((routes) => {
+    for(var i = 0; i < routes.length; i++) {
+      try {
+        routes[i].mount(router)
+      }
+      catch(error) {
+        return cb(error, null)
+      }
+      cb(null, router)
+    }
+  }).catch(error => {
+    cb(error, null)
+  })
+}
+export async function createExpressRouterAsync(path: string) {
+  var router = express.Router()
+  
+  var routes = await parseFile(path)
+  
+  for(var i = 0; i < routes.length; i++) {
+    routes[i].mount(router)
+  }
+  
+  return router
+}
