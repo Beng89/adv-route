1a3a497b60fa3e9db8651546cb4b34844437b452
Ben Goltz <ben.goltz.89@gmail.com>
2016-04-13 22:15:18 -0500

    Updated unit tests to use the generated files and added tests for the async router and app creation methods.

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,10 @@
 var mocha = require("mocha")
 var should = require("chai").should()
 var express = require("express")
-var module = require("..")
+var module = require("../bin")
 
 describe("parseRoute", function() {
-  var parseRoute = require("../lib/parse-route")
+  var parseRoute = require("../bin/lib/parse-route").parseRoute
   
   it("should throw an exception when there are not enough tokens", function(done) {
     should.throw(parseRoute.bind(null, "only two"))
@@ -55,24 +55,9 @@ describe("parseRoute", function() {
     })
   })
   
-  
-  
-  it("should ignore white space", function(done) {
-    should.not.throw(function() {
-      var route = parseRoute("  get     /myroute controller.method")
-      
-      should.equal(route.method, "get")
-      should.equal(route.path, "/myroute")
-      should.equal(route.controller, process.cwd() + "/controller")
-      should.equal(route.target, "method")
-      
-      done()
-    })
-  })
-  
   it("should succeed when the method token is missing", function(done) {
     should.not.throw(function() {
-      var route = parseRoute("  get     /myroute controller")
+      var route = parseRoute("get /myroute controller")
       
       should.equal(route.method, "get")
       should.equal(route.path, "/myroute")
@@ -86,7 +71,7 @@ describe("parseRoute", function() {
 
 
 describe("route.mount", function() {
-  var parseRoute = require("../lib/parse-route")
+  var parseRoute = require("../bin/lib/parse-route").parseRoute
   var router = express.Router()
   
   it("should throw an exception while mounting if the controller cannot be resolved", function(done) {
@@ -135,92 +120,97 @@ describe("route.mount", function() {
 })
 
 describe("parseFile", function() {
-  var parseFile = require("../lib/parse-file")
-  it("should provide an error in the cb method when the file does not exist", function(done) {
-    function cb(err, routes) {
-      should.exist(err)
-      should.equal(err.message, "file " + process.cwd() + "/bad_file does not exist")
-      done()
-    }
-    
-    should.not.throw(parseFile.bind(null, "bad_file", cb))
+  var parseFile = require("../bin/lib/parse-file").parseFile
+  it("should reject the promise when the file does not exist", function(done) {
+    should.not.throw(() => {
+      parseFile("bad_file").then(routes => {
+        should.not.exist(routes)
+      }).catch(error => {
+        should.exist(error)
+        done()
+      })
+    })
   })
   
   it("should ignore empty lines", function(done) {
-    function cb(err, routes) {
-      should.not.exist(err)
-      should.equal(routes.length == 6, true)
-
-      done()
-    }
-    
-    should.not.throw(parseFile.bind(null, "tests/route-files/whitespace", cb))
+    should.not.throw(() => {
+      parseFile("tests/route-files/whitespace").then(routes => {
+        should.exist(routes)
+        should.equal(routes.length == 6, true)
+        done()
+      }).catch(error => {
+        should.not.exist(error)
+      })
+    })
   })
   
   it("should ignore lines that start with #", function(done) {
-    function cb(err, routes) {
-      should.not.exist(err)
-      should.equal(routes.length == 7, true)
-
-      done()
-    }
-    
-    should.not.throw(parseFile.bind(null, "tests/route-files/comments", cb))
+    should.not.throw(() => {
+      parseFile("tests/route-files/comments").then(routes => {
+        should.equal(routes.length == 7, true)
+        done()
+      }).catch(error => {
+        should.not.exist(error)
+      })
+    })
   })
   
   it("should not ignore routes that contain #", function(done) {
-    function cb(err, routes) {
-      should.not.exist(err)
-      should.equal(routes[0].path, '/#/see')
-
-      done()
-    }
-    
-    should.not.throw(parseFile.bind(null, "tests/route-files/comments", cb))
+    should.not.throw(() => {
+      parseFile("tests/route-files/comments").then(routes => {
+        should.equal(routes[0].path, "/#/see")
+        done()
+      }).catch(error => {
+        should.not.exist(error)
+      })
+    })
   })
   
   it("should report the correct line number that an error occurs on", function(done) {
-    function cb(err, routes) {
-      should.exist(err)
-      should.equal(err.message, "line 4: incorrect number of tokens 17; routes require a method, a path and a controller")
-      done()
-    }
-    
-    should.not.throw(parseFile.bind(null, "tests/route-files/line-4-bad", cb))
+    should.not.throw(() => {
+      parseFile("tests/route-files/line-4-bad").then(routes => {
+        should.not.exist(routes)
+      }).catch(error => {
+        should.exist(error)
+        should.equal(error.message, "line 4: incorrect number of tokens 17; routes require a method, a path and a controller")
+        done()
+      })
+    })
   })
   
   it("should correctly parse a file if it exists", function(done) {
-    function cb(err, routes) {
-      should.not.exist(err)
-      should.equal(routes.length == 3, true)
-      
-      should.equal(routes[0].method, "get")
-      should.equal(routes[0].path, "/less")
-      should.equal(routes[0].controller, process.cwd() + "/tests/controllers/less")
-      should.not.exist(routes[0].target)
-      
-      should.equal(routes[1].method, "post")
-      should.equal(routes[1].path, "/more")
-      should.equal(routes[1].controller, process.cwd() + "/tests/controllers/more")
-      should.equal(routes[1].target, "evenMore")
-      
-      should.equal(routes[2].method, "delete")
-      should.equal(routes[2].path, "/enough")
-      should.equal(routes[2].controller, process.cwd() + "/tests/controllers/enough")
-      should.not.exist(routes[2].target);
-      
-      done()
-    }
-
-    should.not.throw(parseFile.bind(null, "tests/route-files/valid", cb))
+    should.not.throw(() => {
+      parseFile("tests/route-files/valid").then(routes => {
+        should.equal(routes.length == 3, true)
+        
+        should.equal(routes[0].method, "get")
+        should.equal(routes[0].path, "/less")
+        should.equal(routes[0].controller, process.cwd() + "/tests/controllers/less")
+        should.not.exist(routes[0].target)
+        
+        should.equal(routes[1].method, "post")
+        should.equal(routes[1].path, "/more")
+        should.equal(routes[1].controller, process.cwd() + "/tests/controllers/more")
+        should.equal(routes[1].target, "evenMore")
+        
+        should.equal(routes[2].method, "delete")
+        should.equal(routes[2].path, "/enough")
+        should.equal(routes[2].controller, process.cwd() + "/tests/controllers/enough")
+        should.not.exist(routes[2].target);
+        
+        done()
+      }).catch(error => {
+        should.not.exist(error)
+      })
+    })
   })
 })
 
 describe("createExpressApp", function() {
-  var createExpressApp = require("../lib/create-express-app")
+  var createExpressApp = require("../bin/lib/create-express-app").createExpressApp
   
   it("should create an express app", function(done) {
-    createExpressApp("tests/route-files/valid", (err, app) => {      
+    createExpressApp("tests/route-files/valid", (err, app) => {   
       should.not.exist(err)
       should.exist(app)
       
@@ -230,14 +220,39 @@ describe("createExpressApp", function() {
 })
 
 describe("createExpressRouter", function() {
-  var createExpressRouter = require("../lib/create-express-router")
+  var createExpressRouter = require("../bin/lib/create-express-router").createExpressRouter
+  
+  it("should create an express router", function(done) {
+    createExpressRouter("tests/route-files/valid", (err, router) => {   
+      should.not.exist(err)
+      should.exist(router)
+      done()
+    })
+  })
+})
+
+describe("createExpressAppAsync", function() {
+  var createExpressAppAsync = require("../bin/lib/create-express-app").createExpressAppAsync
   
   it("should create an express app", function(done) {
-    createExpressRouter("tests/route-files/valid", (err, app) => {      
-      should.not.exist(err)
+    createExpressAppAsync("tests/route-files/valid").then(app => {
       should.exist(app)
-      
       done()
+    }).catch(error => {
+      should.not.exist(error)
+    })
+  })
+})
+
+describe("createExpressRouterAsync", function() {
+  var createExpressRouterAsync = require("../bin/lib/create-express-router").createExpressRouterAsync
+  
+  it("should create an express router", function(done) {
+    createExpressRouterAsync("tests/route-files/valid").then(router => {
+      should.exist(router)
+      done()
+    }).catch(error => {
+      should.not.exist(error)
     })
   })
 })
