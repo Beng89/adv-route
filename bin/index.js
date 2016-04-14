9053adc5ff0e8284c90e6801b8db80512c8911f0
Ben Goltz <ben.goltz.89@gmail.com>
2016-04-13 21:50:37 -0500

    removed an unwanted file.

--- bin/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./lib/create-express-app"));
-__export(require("./lib/create-express-router"));
-__export(require("./lib/parse-file"));
-__export(require("./lib/parse-route"));
-__export(require("./lib/route"));
